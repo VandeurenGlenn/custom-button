0a3b2e7e350839795350d3c6a7625569ee33ad1b
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-12-30 13:55:01 +0100

    :unamused: Fix wrong path to import

--- custom-button.js
+++ custom-button.js
@@ -1,6 +1,6 @@
 import { define } from '../backed/src/utils.js';
-import LitMixin from '../backed/mixins/src/lit-mixin.js';
-import PropertyMixin from '../backed/mixins/src/property-mixin.js';
+import LitMixin from '../backed/src/mixins/lit-mixin.js';
+import PropertyMixin from '../backed/src/mixins/property-mixin.js';
 import '../custom-ripple/custom-ripple.js';
 /**
 * @extends LitMixin
